371408b7ccafc94d14eb4621ac472a9e0079965f
Aryan Thapa <aryanthapa748@gmail.com>
2024-09-07 12:42:11 -0400

    Creating html layout for the list items including delete, moveUp and moveDown buttons

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -2,11 +2,11 @@ import { useState } from "react";
 
 function ToDoList(){
 
-    const [tasks, setTasks] = useState([]);
+    const [tasks, setTasks] = useState(["Eat Breakfast", "Do coding"]);
     const [newTask, setNewTask] = useState('');
 
     function handleInputChange(event){
-
+        setNewTask(event.target.value);
     }
 
     function addTask(){
@@ -31,18 +31,33 @@ function ToDoList(){
             <h1>To-Do-List</h1>
 
             <div>
-                <input type="text" placeholder="Enter a task..." value={newTask}/>
-
+                <input type="text" placeholder="Enter a task..." value={newTask} onChange={handleInputChange}/>
+                <button className="add-button" onClick={addTask}>Add</button>
             </div>
 
-
-
+            <ol>
+                {tasks.map((task, index) => 
+                <li key={index}>
+                    <span className="text">{task}</span>
+                    <button 
+                    className="delete-button"
+                    onClick={() => deleteTask(index)}>
+                    Delete
+                    </button>
+                    <button 
+                    className="move-button"
+                    onClick={() => moveTaskUp(index)}>
+                    👆
+                    </button>
+                    <button 
+                    className="move-button"
+                    onClick={() => moveTaskDown(index)}>
+                    👇
+                    </button>
+                </li>)}
+            </ol>
         </div>
-    
-
     );
-
-
 }
 
 export default ToDoList
